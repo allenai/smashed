87ca2df670fa87fbc9387a3abb9efdccc9742c12
kyleclo <kyleclo@uw.edu>
2022-07-25 23:04:33 -0700

    add some tests for tokenize mapper

--- tests/test_tokenize_mappers.py
+++ tests/test_tokenize_mappers.py
@@ -1,7 +1,8 @@
 import unittest
 
-from smashed.mappers.tokenize import ValidUnicodeMapper
-from smashed.interfaces.simple import Dataset, ValidUnicodeMapper
+import transformers
+from smashed.mappers.tokenize import TokenizerMapper, ValidUnicodeMapper
+from smashed.interfaces.simple import Dataset, TokenizerMapper, ValidUnicodeMapper
 
 class TestValidUnicodeMapper(unittest.TestCase):
     def test_map(self):
@@ -19,3 +20,69 @@ class TestValidUnicodeMapper(unittest.TestCase):
         self.assertListEqual(new_dataset, [
             {'tokens': ['This', 'example', 'has', 'bad', '[UNK]', '[UNK]', 'Modalities\uf02a']}
         ])
+
+
+class TestTokenizerMapper(unittest.TestCase):
+    def setUp(self):
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            'allenai/scibert_scivocab_uncased'
+        )
+
+    def test_map(self):
+        mapper = TokenizerMapper(
+            input_field='text',
+            tokenizer=self.tokenizer
+        )
+        dataset = Dataset([
+            {
+                'text': [
+                    'This is a sentence.',
+                    'This is two sentences. Here is the second one.'
+                ]
+            },
+            {
+                'text': [
+                    'This is a separate instance.',
+                    'This',
+                    'is',
+                    'some',
+                    'tokens',
+                    '.'
+                ]
+            }
+        ])
+        new_dataset = mapper.map(dataset)
+        assert len(dataset) == len(new_dataset)     # same num dicts
+        assert isinstance(new_dataset[0], dict)     # each element is dict
+        assert 'input_ids' in new_dataset[0]        # dict has keys
+        assert 'attention_mask' in new_dataset[0]
+        assert len(new_dataset[0]['attention_mask']) == len(new_dataset[0]['input_ids'])    # mask same dimension as inputs
+        assert self.tokenizer.decode(new_dataset[0]['input_ids'][0]) == '[CLS] this is a sentence. [SEP]'
+        assert self.tokenizer.decode(new_dataset[0]['input_ids'][1]) == '[CLS] this is two sentences. here is the second one. [SEP]'
+
+    def test_truncation_max_length(self):
+        mapper = TokenizerMapper(
+            input_field='text',
+            tokenizer=self.tokenizer,
+            truncation=True,
+            max_length=10,
+        )
+        dataset = Dataset([
+            {
+                'text': [
+                    'This is an instance that will be truncated because it is longer than ten word pieces.',
+                    'This is the subsequent unit in this instance that will be separately truncated.'
+                ]
+            },
+            {
+                'text': [
+                    'This is the next instance.'
+                ]
+            }
+        ])
+        new_dataset = mapper.map(dataset)
+        assert len(dataset) == len(new_dataset)     # same num dicts
+        assert self.tokenizer.decode(new_dataset[0]['input_ids'][0]) == '[CLS] this is an instance that will be truncated [SEP]'
+        assert self.tokenizer.decode(new_dataset[0]['input_ids'][1]) == '[CLS] this is the subsequent unit in this instance [SEP]'
+        assert self.tokenizer.decode(new_dataset[1]['input_ids'][0]) == '[CLS] this is the next instance. [SEP]'
+
