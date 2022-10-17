7c544dc5ab694a6414d60c62a59484ffb0dd7ab0
Luca Soldaini <lucas@allenai.org>
2022-10-17 09:50:13 -0700

    added SingleSequenceStriderMapper + tests

--- src/smashed/mappers/__init__.py
+++ src/smashed/mappers/__init__.py
@@ -31,7 +31,7 @@ from .prompting import (
     FillTextPromptMapper,
     TruncateNFieldsMapper,
 )
-from .shape import FlattenMapper, UnpackingMapper
+from .shape import FlattenMapper, UnpackingMapper, SingleSequenceStriderMapper
 from .text import FtfyMapper
 from .tokenize import PaddingMapper, TokenizerMapper, ValidUnicodeMapper
 from .types import BinarizerMapper, CastMapper, LookupMapper, OneHotMapper
@@ -67,6 +67,7 @@ __all__ = [
     "Python2TorchMapper",
     "RenameFieldsMapper",
     "SequencesConcatenateMapper",
+    "SingleSequenceStriderMapper",
     "SingleValueToSequenceMapper",
     "StartCachingMapper",
     "TensorCollatorMapper",
