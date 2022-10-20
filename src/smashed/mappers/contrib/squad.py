72b4f15a9ba3f9c503f8817fc2d416eff1d4a5bc
Luca Soldaini <lucas@allenai.org>
2022-10-20 15:50:19 -0700

    improved context encoder

--- src/smashed/mappers/contrib/squad.py
+++ src/smashed/mappers/contrib/squad.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from smashed.base import SingleBaseMapper, TransformElementType
 
 
@@ -17,22 +18,48 @@ class ConcatenateContextMapper(SingleBaseMapper):
     def __init__(
         self,
         context_field_name: str = "context",
-        new_line_break: str = "\n",
-        new_para_break: str = "\n\n",
+        section_bos: str = "",
+        section_eos: str = "\n",
+        paragraph_bos: str = "\n\n",
+        paragraph_eos: str = "\n",
+        header_bos: Optional[str] = None,
+        header_eos: Optional[str] = None,
+        document_bos: str = '',
+        document_eos: str = '',
     ):
         """
         Args:
-            context_field_name (str, optional): Name of the field containing
-                the context. Defaults to "context".
-            new_line_break (str, optional): String to use to separate lines
-                in the context paragraphs. Defaults to "\\n".
-            new_para_break (str, optional): String to use to separate
-                paragraphs in the context. Defaults to "\\n\\n".
+            context_field_name (str): the name of the field containing the
+                context. Defaults to "context".
+            section_bos (str): the beginning of section token. Defaults to ""
+                (empty string).
+            section_eos (str): the end of section token. Defaults to "\\n"
+                (a single new line).
+            paragraph_bos (str): the beginning of paragraph token.
+                Defaults to "\\n\\n" (two new lines).
+            paragraph_eos (str): the end of paragraph token. Defaults to "\\n"
+                (a single new line).
+            header_bos (str, optional): the beginning of header token; if None,
+                the same as section_bos. Defaults to None.
+            header_eos (str, optional): the end of header token; if None,
+                the same as section_eos. Defaults to None.
+            document_bos (str): the beginning of document token. Defaults to
+                '' (empty string).
+            document_eos (str): the end of document token. Defaults to ''
+                (empty string).
+
         """
 
         self.context_field_name = context_field_name
-        self.new_line_break = new_line_break
-        self.new_para_break = new_para_break
+        self.sec_bos = section_bos
+        self.sec_eos = section_eos
+        self.par_bos = paragraph_bos
+        self.par_eos = paragraph_eos
+        self.hdr_bos = header_bos or self.par_bos
+        self.hdr_eos = header_eos or self.par_eos
+        self.doc_bos = document_bos
+        self.doc_eos = document_eos
+
         super().__init__(
             input_fields=[context_field_name],
             output_fields=[context_field_name],
@@ -42,12 +69,26 @@ class ConcatenateContextMapper(SingleBaseMapper):
         if isinstance(data["context"], str):
             return data
         elif isinstance(data["context"], list):
-            data["context"] = self.new_para_break.join(
-                self.new_line_break.join(section)
-                if isinstance(section, list)
-                else section
-                for section in data["context"]
-            )
+            sections = []
+            for sec in data["context"]:
+                if sec is None:
+                    continue
+
+                elif isinstance(sec, str):
+                    sections.append(self.sec_bos + sec + self.sec_eos)
+
+                elif isinstance(sec, list):
+                    for i, para in enumerate(sec):
+                        if para is None:
+                            continue
+                        if i == 0:
+                            sections.append(self.hdr_bos + para + self.hdr_eos)
+                        else:
+                            sections.append(self.par_bos + para + self.par_eos)
+                else:
+                    raise ValueError(f"Invalid type for section: {type(sec)}")
+
+            data['context'] = self.doc_bos + "".join(sections) + self.doc_eos
             return data
         else:
             raise ValueError(
