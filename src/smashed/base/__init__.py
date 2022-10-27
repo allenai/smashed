80e7c9afd8a414cc53d6da775a52806f294a14e4
Luca Soldaini <lucas@allenai.org>
2022-10-26 21:56:43 -0700

    moved utils part deux

--- src/smashed/base/__init__.py
+++ src/smashed/base/__init__.py
@@ -1,11 +1,13 @@
 from .mappers import BatchedBaseMapper, SingleBaseMapper
+from .pipeline import make_pipeline
 from .recipes import BaseRecipe
 from .types import TransformBatchType, TransformElementType
 
 __all__ = [
-    "SingleBaseMapper",
     "BaseRecipe",
     "BatchedBaseMapper",
-    "TransformElementType",
+    "make_pipeline",
+    "SingleBaseMapper",
     "TransformBatchType",
+    "TransformElementType",
 ]
