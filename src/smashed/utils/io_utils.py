23e1c2814482b40360f86864a6e3381232f01a0b
Luca Soldaini <lucas@allenai.org>
2023-01-11 11:01:30 -0800

    improvements to client

--- src/smashed/utils/io_utils.py
+++ src/smashed/utils/io_utils.py
@@ -1,7 +1,9 @@
-import os
 import shutil
-from contextlib import contextmanager
+from contextlib import ExitStack, contextmanager
 from logging import INFO, Logger, getLogger
+from os import remove as remove_local_file
+from os import stat as stat_local_file
+from os import walk as local_walk
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import (
@@ -13,15 +15,17 @@ from typing import (
     Generator,
     Iterable,
     Optional,
+    TypeVar,
     Union,
 )
-from urllib.parse import urlparse
+from urllib.parse import ParseResult, urlparse
 
 from necessary import Necessary, necessary
 
 with necessary("boto3", soft=True) as BOTO_AVAILABLE:
     if TYPE_CHECKING or BOTO_AVAILABLE:
         import boto3
+        from botocore.client import BaseClient
 
 
 __all__ = [
@@ -31,11 +35,49 @@ __all__ = [
     "remove_directory",
 ]
 
+PathType = Union[str, Path, ParseResult]
+ClientType = Union[BaseClient, None]
+
+
+def uri_stringify(uri: PathType) -> str:
+    """Convert a URI to a string."""
+    if isinstance(uri, str):
+        return uri
+
+    if isinstance(uri, Path):
+        return str(uri)
+
+    if isinstance(uri, ParseResult):
+        return uri.geturl()
+
+
+def join_uri(*uris: PathType) -> str:
+    """Join a URI."""
+    first, *rest, last = map(uri_stringify, uris)
+    rest = [part.strip("/") for part in rest]
+    return "/".join([first.rstrip("/"), *rest, last.lstrip("/")])
+
 
 def get_logger() -> Logger:
     """Get the default logger for this module."""
     (logger := getLogger(__file__)).setLevel(INFO)
     return logger
+
+
+def get_client_if_needed(path: PathType) -> ClientType:
+
+    parse = (
+        urlparse(uri_stringify(path))
+        if not isinstance(path, ParseResult)
+        else path
+    )
+
+    if parse.scheme == "s3":
+        return boto3.client("s3")  # pyright: ignore
+    elif parse.scheme == "file" or parse.scheme == "":
+        return None  # pyright: ignore
+    else:
+        raise ValueError(f"Unsupported scheme {parse.scheme}")
 
 
 @Necessary("boto3")
@@ -46,6 +88,7 @@ def open_file_for_read(
     open_fn: Optional[Callable] = None,
     logger: Optional[Logger] = None,
     open_kwargs: Optional[Dict[str, Any]] = None,
+    client: Optional[ClientType] = None,
 ) -> Generator[IO, None, None]:
     """Get a context manager to read in a file that is either on
     S3 or local.
@@ -71,7 +114,9 @@ def open_file_for_read(
     assert "r" in mode, "Only read mode is supported"
 
     if parse.scheme == "s3":
-        client = boto3.client("s3")
+        client = client or get_client_if_needed(path)
+        assert client is not None, "Could not get S3 client"
+
         logger.info(f"Downloading {path} to a temporary file")
         with NamedTemporaryFile(delete=False) as f:
             path = f.name
@@ -87,7 +132,7 @@ def open_file_for_read(
             yield f
     finally:
         if remove:
-            os.remove(path)
+            remove_local_file(path)
 
 
 @Necessary("boto3")
@@ -99,6 +144,7 @@ def open_file_for_write(
     open_fn: Optional[Callable] = None,
     logger: Optional[Logger] = None,
     open_kwargs: Optional[Dict[str, Any]] = None,
+    client: Optional[ClientType] = None,
 ) -> Generator[IO, None, None]:
     """Get a context manager to write to a file that is either on
     S3 or local.
@@ -116,7 +162,8 @@ def open_file_for_write(
             to the open function. Defaults to None.
     """
 
-    parse = urlparse(str(path))
+    path = str(path)
+    parse = urlparse(path)
     local = None
     logger = logger or get_logger()
     open_fn = open_fn or open
@@ -137,25 +184,28 @@ def open_file_for_write(
                 local = f.name
     finally:
         if local is None:
-            if skip_if_empty and os.stat(path).st_size == 0:
+            if skip_if_empty and stat_local_file(path).st_size == 0:
                 logger.info(f"Skipping empty file {path}")
-                os.remove(path)
+                remove_local_file(path)
         elif parse.scheme == "s3":
             dst = f'{parse.netloc}{parse.path.lstrip("/")}'
-            if skip_if_empty and os.stat(local).st_size == 0:
+            if skip_if_empty and stat_local_file(local).st_size == 0:
                 logger.info(f"Skipping upload to {dst} since {local} is empty")
             else:
                 logger.info(f"Uploading {local} to {dst}")
-                client = boto3.client("s3")
+                client = client or get_client_if_needed(path)
+                assert client is not None, "Could not get S3 client"
                 client.upload_file(local, parse.netloc, parse.path.lstrip("/"))
-            os.remove(local)
+            remove_local_file(local)
         else:
             raise ValueError(f"Unsupported scheme {parse.scheme}")
 
 
 @Necessary("boto3")
 def recursively_list_files(
-    path: Union[str, Path], ignore_hidden_files: bool = True
+    path: Union[str, Path],
+    ignore_hidden_files: bool = True,
+    client: Optional[ClientType] = None,
 ) -> Iterable[str]:
     """Recursively list all files in the given directory on network prefix
 
@@ -166,15 +216,18 @@ def recursively_list_files(
             (i.e. files that start with a dot) when listing. Defaults to True.
     """
 
-    parse = urlparse(str(path))
+    path = str(path)
+    parse = urlparse(path)
 
     if parse.scheme == "s3":
-        cl = boto3.client("s3")
+        client = client or get_client_if_needed(path)
+        assert client is not None, "Could not get S3 client"
+
         prefixes = [parse.path.lstrip("/")]
 
         while len(prefixes) > 0:
             prefix = prefixes.pop()
-            paginator = cl.get_paginator("list_objects_v2")
+            paginator = client.get_paginator("list_objects_v2")
             pages = paginator.paginate(Bucket=parse.netloc, Prefix=prefix)
             for page in pages:
                 for obj in page["Contents"]:
@@ -184,11 +237,11 @@ def recursively_list_files(
                         yield f's3://{parse.netloc}/{obj["Key"]}'
 
     elif parse.scheme == "file" or parse.scheme == "":
-        for root, _, files in os.walk(parse.path):
+        for root, _, files in local_walk(parse.path):
             for f in files:
                 if ignore_hidden_files and f.startswith("."):
                     continue
-                yield os.path.join(root, f)
+                yield join_uri(root, f)
     else:
         raise NotImplementedError(f"Unknown scheme: {parse.scheme}")
 
@@ -197,8 +250,10 @@ def recursively_list_files(
 def copy_directory(
     src: Union[str, Path],
     dst: Union[str, Path],
-    ignore_hidden_files: bool = True,
+    ignore_hidden_files: bool = False,
+    skip_if_empty: bool = False,
     logger: Optional[Logger] = None,
+    client: Optional[ClientType] = None,
 ):
     """Copy a directory from one location to another. Source or target
     locations can be local, remote, or a mix of both.
@@ -215,38 +270,73 @@ def copy_directory(
 
     logger = logger or get_logger()
 
-    src = Path(src)
-    dst = Path(dst)
-
+    # we convert to string because the Path library does not handle
+    # well network locations.
+    src = str(src)
+    dst = str(dst)
     cnt = 0
 
+    client = client or get_client_if_needed(src) or get_client_if_needed(dst)
+
     for source_path in recursively_list_files(
-        src, ignore_hidden_files=ignore_hidden_files
+        str(src), ignore_hidden_files=ignore_hidden_files
     ):
-        destination = dst / Path(source_path).relative_to(src)
+        # we strip the segment of source_path that is the common prefix in src,
+        # then join the remaining bit
+        destination = join_uri(dst, source_path[len(src) :])
 
         logger.info(f"Copying {source_path} to {destination}; {cnt:,} so far")
 
-        with open_file_for_read(source_path, mode="rb") as s:
-            with open_file_for_write(destination, mode="wb") as d:
-                d.write(s.read())
+        with ExitStack() as stack:
+            s = stack.enter_context(
+                open_file_for_read(source_path, mode="rb", client=client)
+            )
+            d = stack.enter_context(
+                open_file_for_write(
+                    destination,
+                    mode="wb",
+                    skip_if_empty=skip_if_empty,
+                    client=client,
+                )
+            )
+            d.write(s.read())
 
         cnt += 1
 
 
 @Necessary("boto3")
-def remove_directory(path: Union[str, Path]):
+def remove_file(path: Union[str, Path], client: Optional[ClientType] = None):
+    """Remove a file at the provided path."""
+
+    path = str(path)
+    parse = urlparse(path)
+
+    if parse.scheme == "s3":
+        client = client or get_client_if_needed(path)
+        assert client is not None, "Could not get S3 client"
+        client.delete_object(Bucket=parse.netloc, Key=parse.path.lstrip("/"))
+    elif parse.scheme == "file" or parse.scheme == "":
+        remove_local_file(path)
+    else:
+        raise NotImplementedError(f"Unknown scheme: {parse.scheme}")
+
+
+@Necessary("boto3")
+def remove_directory(
+    path: Union[str, Path], client: Optional[ClientType] = None
+):
     """Completely remove a directory at the provided path."""
 
     parse = urlparse(str(path))
 
     if parse.scheme == "s3":
-        client = boto3.client("s3")
-        for fn in recursively_list_files(path, ignore_hidden_files=False):
-            parsed = urlparse(str(fn))
-            client.delete_object(
-                Bucket=parsed.netloc, Key=parsed.path.lstrip("/")
-            )
+        client = client or get_client_if_needed(path)
+        assert client is not None, "Could not get S3 client"
+
+        for fn in recursively_list_files(
+            path=path, ignore_hidden_files=False, client=client
+        ):
+            remove_file(fn, client=client)
     elif parse.scheme == "file" or parse.scheme == "":
         shutil.rmtree(path, ignore_errors=True)
     else:
